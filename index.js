d2a9c5ca4f21db77c6fc54d50bbcd7b5ed6ec159
Gyzelle Muthoni <gyznjeri06@gmail.com>
2024-04-11 11:57:51 +0300

    phase-1-destructuring-assignment

--- index.js
+++ index.js
@@ -29,20 +29,37 @@ const nestedMuppet = {
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
 
+const { moo, neigh, baa, oink, cluck } = { moo: "cow", neigh: "horse", baa: "sheep", oink: "pig", cluck: "chicken" };
+console.log(moo, neigh, baa, oink, cluck);
+
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
 
+const { bessie, dolly, babe, little } = { bessie: "cow", dolly: "sheep", babe: "pig", little: "chicken" };
+console.log(bessie, dolly, babe, little);
+
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
+const { blackAndWhite, black, pink } = { blackAndWhite: "cow", black: "sheep", pink: "pig" };
+console.log(blackAndWhite, black, pink);
 
 // Arrays
 
 // 4. Use destructuring to assign appropriate variables using the color names.
 
+const [red, orange, yellow, green, blue,violet] = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+console.log(red, orange, yellow, green, blue, violet);
+
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
 
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+const [, , , , , indg] = ["red", "orange", "yellow", "green", "blue", "indigo"];
+console.log(indg);
 
 // Objects
 
 // 7. Use destructuring to assign all variables using the keys as the variable names
 
-// 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+// 8. Use destructuring to assign songs 2 and 4, and Kermit's job and part
+
+const { song2, song4,KermitsJob, Part } = 
+{ song2: "Moving Right Along", song4: "I Hope That Something Better Comes Along", nestedJob: "Host of The Muppet Show", nestedPartner: "Miss Piggy" };
+console.log(song2, song4, nestedJob, nestedPartner); 
